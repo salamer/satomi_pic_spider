5bd99a6cf347433decadafb222262e227b1e7192
salamer <salameryy@163.com>
2015-10-12 20:55:56 +0800

    ~~

---
+++ satomi_pic/spiders/masami_spider.py
@@ -0,0 +1,32 @@
+from scrapy.spiders import Rule,CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+
+from satomi_pic.items import SatomiPicItem
+
+class SatomiSpider(CrawlSpider):
+	name="masami_pic_spider"
+
+	download_delay=1
+
+	allowed_domains=[]
+
+	start_urls=[
+		'http://movie.douban.com/celebrity/1018667/photo/2154919502/'
+	]
+
+	rules=(
+		Rule(LinkExtractor(allow=(r'http://movie.douban.com/celebrity/1018667/photo/\d+')),callback='parse_item',follow=True),
+
+	)
+
+	def parse_item(self,response):
+		print response
+
+		sel=Selector(response)
+
+		item=SatomiPicItem()
+
+		item['image_urls']=sel.xpath('//div[@class="photo-show"]/div[@class="photo-wp"]/a/img/@src').extract()
+
+		yield item
